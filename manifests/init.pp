1910b8089ff8d314d456b2452551833d6c3f16be
jayfitz <j.fitzsimmons@kainos.com>
2017-05-17 09:48:57 +0100

    prefixing vars with llc_

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,44 +12,44 @@
 #
 
 class opendj (
-  $ldap_port              = hiera('ldap_port'),
-  $ldaps_port             = hiera('ldaps_port'),
-  $opendj_admin_port      = hiera('opendj_admin_port'),
-  $opendj_repl_port       = hiera('opendj_repl_port'),
-  $opendj_jmx_port        = hiera('opendj_jmx_port'),
-  $opendj_admin_user      = hiera('opendj_admin_user'),
-  $opendj_admin_password  = hiera('opendj_admin_password'),
-  $opendj_base_dn         = hiera('opendj_base_dn'),
-  $opendj_base_dir        = hiera('opendj_base_dir'),
-  $opendj_home            = hiera('opendj_home'),
-  $opendj_user            = hiera('opendj_user'),
-  $opendj_group           = hiera('opendj_group'),
-  $opendj_host            = hiera('opendj_host'),
-  $opendj_tmp             = hiera('opendj_tmpdir'),
-  $opendj_master          = hiera('opendj_master'),
-  $opendj_url             = hiera('opendj_url'),
-  $ldap_base_dn           = hiera('ldap_base_dn'),
-  $ldap_auth_org01        = hiera('ldap_auth_org01'),
-  $ldap_auth_org02        = hiera('ldap_auth_org02'),
-  $ldap_roles_dn          = hiera('ldap_roles_dn'),
-  $ldap_group_clients     = hiera('ldap_group_clients'),
-  $ldap_group_laadmins    = hiera('ldap_group_laadmins'),
-  $ldap_group_lausers     = hiera('ldap_group_lausers'),
-  $ldap_group_lradmins    = hiera('ldap_group_lradmins'),
-  $ldap_group_lrusers     = hiera('ldap_group_lrusers'),
-  $ldap_people_dn         = hiera('ldap_people_dn'),
-  $opendj_ldap_content    = hiera('opendj_ldap_content')
+  $ldap_port              = hiera('llc_ldap_port'),
+  $ldaps_port             = hiera('llc_ldaps_port'),
+  $opendj_admin_port      = hiera('llc_opendj_admin_port'),
+  $opendj_repl_port       = hiera('llc_opendj_repl_port'),
+  $opendj_jmx_port        = hiera('llc_opendj_jmx_port'),
+  $opendj_admin_user      = hiera('llc_opendj_admin_user'),
+  $opendj_admin_password  = hiera('llc_opendj_admin_password'),
+  $opendj_base_dn         = hiera('llc_opendj_base_dn'),
+  $opendj_base_dir        = hiera('llc_opendj_base_dir'),
+  $opendj_home            = hiera('llc_opendj_home'),
+  $opendj_user            = hiera('llc_opendj_user'),
+  $opendj_group           = hiera('llc_opendj_group'),
+  $opendj_host            = hiera('llc_opendj_host'),
+  $opendj_tmp             = hiera('llc_opendj_tmpdir'),
+  $opendj_master          = hiera('llc_opendj_master'),
+  $opendj_url             = hiera('llc_opendj_url'),
+  $ldap_base_dn           = hiera('llc_ldap_base_dn'),
+  $ldap_auth_org01        = hiera('llc_ldap_auth_org01'),
+  $ldap_auth_org02        = hiera('llc_ldap_auth_org02'),
+  $ldap_roles_dn          = hiera('llc_ldap_roles_dn'),
+  $ldap_group_clients     = hiera('llc_ldap_group_clients'),
+  $ldap_group_laadmins    = hiera('llc_ldap_group_laadmins'),
+  $ldap_group_lausers     = hiera('llc_ldap_group_lausers'),
+  $ldap_group_lradmins    = hiera('llc_ldap_group_lradmins'),
+  $ldap_group_lrusers     = hiera('llc_ldap_group_lrusers'),
+  $ldap_people_dn         = hiera('llc_ldap_people_dn'),
+  $opendj_ldap_content    = hiera('llc_opendj_ldap_content')
 ) {
   $common_opts   = "-h localhost -D '${opendj::opendj_admin_user}' -w ${opendj::opendj_admin_password}"
   $ldapsearch    = "${opendj::opendj_home}/bin/ldapsearch ${common_opts} -p ${opendj::ldap_port}"
   $ldapmodify    = "${opendj::opendj_home}/bin/ldapmodify ${common_opts} -p ${opendj::ldap_port}"
   $dsconfig      = "${opendj::opendj_home}/bin/dsconfig \
-    --noPropertiesFile --no-prompt --port '${opendj_admin_port}' --trustAll  --bindDN '${opendj_admin_user}' \
-    --bindPassword '${opendj_admin_password}'"
-  $dsreplication = "${opendj::opendj_home}/bin/dsreplication --adminUID admin --adminPassword ${opendj_admin_password} -X -n"
+    --noPropertiesFile --no-prompt --port '${llc_opendj_admin_port}' --trustAll  --bindDN '${llc_opendj_admin_user}' \
+    --bindPassword '${llc_opendj_admin_password}'"
+  $dsreplication = "${opendj::opendj_home}/bin/dsreplication --adminUID admin --adminPassword ${llc_opendj_admin_password} -X -n"
 # props_file Contains passwords, thus (temporarily) stored in /dev/shm
   $props_file    = '/dev/shm/opendj.properties'
-  $opendj_dirs = [ "${opendj_home}","${opendj_home}/locks","${opendj_home}/logs"]
+  $opendj_dirs = [ "${llc_opendj_home}","${llc_opendj_home}/locks","${llc_opendj_home}/logs"]
 
   if ! defined(Package['java-1.8.0-openjdk']) {
     package { 'java-1.8.0-openjdk':
@@ -58,16 +58,16 @@ class opendj (
   }
 
   exec{'retrieve_opendj_zip':
-    command => "${opendj_url}",
-    creates => "${opendj_base_dir}/opendj.zip",
+    command => "${llc_opendj_url}",
+    creates => "${llc_opendj_base_dir}/opendj.zip",
     notify => exec["unzip_opendj"],
   }
 
   exec { 'unzip_opendj':
     require     => Exec["retrieve_opendj_zip"],
-    command     => "/usr/bin/unzip ${opendj_base_dir}/opendj.zip -d ${opendj_base_dir}/",
+    command     => "/usr/bin/unzip ${llc_opendj_base_dir}/opendj.zip -d ${llc_opendj_base_dir}/",
     user        => 'root',
-    creates     => "${opendj_home}/setup",
+    creates     => "${llc_opendj_home}/setup",
     refreshonly => true,
   }
   
@@ -98,7 +98,7 @@ class opendj (
     mode    => '0600',
   }
   ->
-  file { "${opendj_home}/opendj-ldap.ldif":
+  file { "${llc_opendj_home}/opendj-ldap.ldif":
     ensure  => file,
     content => $opendj_ldap_content,
     owner   => $opendj_user,
@@ -108,40 +108,40 @@ class opendj (
   ->
   file_line { 'file_limits_soft':
     path    => '/etc/security/limits.conf',
-    line    => "${opendj_user} soft nofile 65536",
+    line    => "${llc_opendj_user} soft nofile 65536",
   }
   ->
   file_line { 'file_limits_hard':
     path    => '/etc/security/limits.conf',
-    line    => "${opendj_user} hard nofile 131072",
+    line    => "${llc_opendj_user} hard nofile 131072",
   }
   ->
   exec { 'configure opendj1':
-    command => "${opendj_home}/setup --cli -v \
-    --ldapPort '${ldap_port}' \
-    --adminConnectorPort '${opendj_admin_port}' \
-    --rootUserDN '${opendj_admin_user}' \
-    --rootUserPassword '${opendj_admin_password}' \
+    command => "${llc_opendj_home}/setup --cli -v \
+    --ldapPort '${llc_ldap_port}' \
+    --adminConnectorPort '${llc_opendj_admin_port}' \
+    --rootUserDN '${llc_opendj_admin_user}' \
+    --rootUserPassword '${llc_opendj_admin_password}' \
     --no-prompt --noPropertiesFile \
     --doNotStart \
     --generateSelfSignedCertificate \
     --hostname esec-ldap \
     --acceptLicense \
     --enableStartTLS",
-    creates => "${opendj_home}/config",
+    creates => "${llc_opendj_home}/config",
   }
   ->
   exec { 'configure opendj2':
     command => "/opt/opendj/bin/start-ds",
-    creates => "${opendj_home}/config_done",
+    creates => "${llc_opendj_home}/config_done",
   }
   ->
   exec { 'configure opendj3':
     command => "${dsconfig} \
     --hostname localhost create-backend \
     --backend-name userRoot --type=je \
-    --set base-dn:'${ldap_base_dn}' --set enabled:true",
-    creates => "${opendj_home}/config_done",
+    --set base-dn:'${llc_ldap_base_dn}' --set enabled:true",
+    creates => "${llc_opendj_home}/config_done",
   }
   ->
   exec { 'configure opendj4':
@@ -149,28 +149,28 @@ class opendj (
     --set default-password-storage-scheme:PBKDF2 \
     --set password-attribute:userpassword \
     --type password-policy --policy-name password-policy",
-    creates => "${opendj_home}/config_done",
+    creates => "${llc_opendj_home}/config_done",
   }
   ->
   exec { 'configure opendj5':
-    command => "/opt/opendj/bin/import-ldif --includeBranch '${ldap_base_dn}' \
-    --backendID userRoot  --start 0 --port '${opendj_admin_port}' --trustAll \
-    --bindPassword '${opendj_admin_password}' --hostname localhost \
+    command => "/opt/opendj/bin/import-ldif --includeBranch '${llc_ldap_base_dn}' \
+    --backendID userRoot  --start 0 --port '${llc_opendj_admin_port}' --trustAll \
+    --bindPassword '${llc_opendj_admin_password}' --hostname localhost \
     --ldifFile /opt/opendj/opendj-ldap.ldif",
-    creates => "${opendj_home}/config_done",
+    creates => "${llc_opendj_home}/config_done",
   }
   ->
   exec { 'configure opendj6':
     command => '/opt/opendj/bin/stop-ds',
-    creates => "${opendj_home}/config_done",
+    creates => "${llc_opendj_home}/config_done",
   }
   ->
   exec { 'what dependency cycle':
-    command => "chown -R ${opendj_user}:${opendj_group} ${opendj_home}",
+    command => "chown -R ${llc_opendj_user}:${llc_opendj_group} ${llc_opendj_home}",
   }
   ->
   exec { 'create RC script':
-    command => "${opendj_home}/bin/create-rc-script --userName ${opendj_user} \
+    command => "${llc_opendj_home}/bin/create-rc-script --userName ${llc_opendj_user} \
         --outputFile /etc/init.d/opendj",
     creates => '/etc/init.d/opendj',
   }
@@ -186,7 +186,7 @@ class opendj (
     hasstatus  => false,
   }
   ->
-  file { "${opendj_home}/config_done":
+  file { "${llc_opendj_home}/config_done":
     ensure  => file,
     content => "config_done",
     owner   => $opendj_user,
